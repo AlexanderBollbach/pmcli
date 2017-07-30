c2ba93b632c575c5a8a6aae5bb35afc5e3019d0d
AlexanderBollbach <alexander.bollbach@gmail.com>
2017-07-30 14:46:13 -0400

    Debugging

--- commands/GH1.js
+++ commands/GH1.js
@@ -7,6 +7,9 @@ module.exports = function GH1(program) {
 		.command('gh1')
 		.description('fetch ur prs')
 		.action(function(command) {
+
+
+			console.log("running command: gh1");
 				GITHUB.req1(function(response) {
 					console.log(response);
 				})
